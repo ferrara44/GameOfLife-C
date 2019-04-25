6fa1e4fef6ca2b0645d1c4bb0de3cef1fd383dc8
Joseph Mellor <the.landfill.coding@gmail.com>
2019-04-25 12:35:29 -0500

    Fixed some logic errors in my code and added more explanation for my algorithm.

--- Game of Life.c
+++ Game of Life.c
@@ -45,17 +45,47 @@ void printWorld(char world[COLS][ROWS]){ //Prints the next world on screen
 
 void evaluate(int b, int c, char world_a[COLS][ROWS], char world_b[COLS][ROWS]){ //checks how many living neighbors this tilr currently has, and determines the outcome as per the rules of the game.
 	int neighbors = 0;
-	int alive = (world_a[b][c] == 'o');
+	// These nested for loops iterate through the nine squares around the current cell, where X is the current cell.
+	// ----------------------------------------------
+	// |(b - 1, c - 1)|(b + 0, c - 1)|(b + 1, c - 1)|
+	// ----------------------------------------------
+	// |(b - 1, c + 0)|(b + 0, c + 0)|(b + 1, c + 0)|
+	// ----------------------------------------------
+	// |(b - 1, c + 1)|(b + 0, c + 1)|(b + 1, c + 1)|
+	// ----------------------------------------------
 	for (int i = -1; i <= 1; i++) {
 		for (int j = -1; j <= 1; j++) {
-			if (i == j || b + i >= COLS || b + i < 0 || c + j >= ROWS || c + j < 0) {
-				continue;
+			// This line was originally incorrect, my bad. I had i == j, which skips the diagonal.
+			if ((i == 0 && j == 0)				// If we're looking at the current cell
+
+				|| b + i >= COLS				// or the cell we're looking at is outside of the world
+				|| b + i < 0
+				|| c + j >= ROWS
+				|| c + j < 0) 
+			{
+				continue;						// Skip it
 			}
-			neighbors += (world_a[b + i][c + j] == 'o');
+
+			// If the cell is alive, (world_a[b + i][c + j] == 'o') will return 1,
+			// otherwise the expression will return 0.
+			// You could also write it as
+			// if (world_a[b + i][c + j] == 'o') neighbors++;
+			if (world_a[b + i][c + j] == 'o') neighbors++;
 		}
 	}
-	
-	if (neighbors == 3 || (alive && (neighbors == 2))) {
+
+	// I'm going to do some Boolean logic to show that this is equivalent to
+	// the rules of Game of Life
+
+	// Let x = three neighbors, y = two neighbors, z = alive, and !z = dead.	(definition of variables and not alive == dead)
+	// The rules are therefore a cell is alive if and only if
+	// (three neighbors and dead) or (alive and (two or three neighbors))		(definition of the rules of Game of Life)
+	// (x && !z) || (z && (x || y))												(just rewrote it using variables)
+	// == (x && !z) || (x && z) || (y && z)										(distributive property of and/or)
+	// == (x && (!z || z)) || (y && z)											(distributive property of and/or)
+	// == x || (y && z)															(!z || z) == true and x && true == x
+	// == three neighbors || (two neighbors && alive)
+	if (neighbors == 3 || (neighbors == 2 && world_a[b][c] == 'o')) {
 		world_b[b][c] = 'o';
 	} else {
 		world_b[b][c] = ' ';
